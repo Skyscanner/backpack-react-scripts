aa4385178eb0e8e07b064c3f8d0b2df3f019090f
Jayson Wu <jaysonwu@skyscanner.net>
2023-02-07 19:13:50 +0800

    restore command

--- tasks/e2e-simple.sh
+++ tasks/e2e-simple.sh
@@ -148,7 +148,7 @@ publishToLocalRegistry
 
 # Install the app in a temporary location
 cd $temp_app_path
-npx create-react-app test-app --scripts-version=@skyscanner/backpack-react-scripts --template @skyscanner/backpack --use-yarn
+npx create-react-app test-app --scripts-version=@skyscanner/backpack-react-scripts --template @skyscanner/backpack
 
 # TODO: verify we installed prerelease
 
