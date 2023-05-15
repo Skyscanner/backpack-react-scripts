7bddddfee17b6b1f67d1b7f28edafb49e73e1b4c
gc.zhu <gc.zhu@skyscanner.net>
2023-05-15 11:25:48 +0800

    handle relative path

--- packages/react-scripts/backpack-addons/babelIncludePrefixes.js
+++ packages/react-scripts/backpack-addons/babelIncludePrefixes.js
@@ -1,12 +1,19 @@
-"use strict";
+'use strict';
 
-const paths = require("../config/paths");
+const paths = require('../config/paths');
+const path = require('path');
 const appPackageJson = require(paths.appPackageJson);
-const bpkReactScriptsConfig = appPackageJson["backpack-react-scripts"] || {};
+const bpkReactScriptsConfig = appPackageJson['backpack-react-scripts'] || {};
 const customModuleRegexes = bpkReactScriptsConfig.babelIncludePrefixes
-  ? bpkReactScriptsConfig.babelIncludePrefixes.map(
-      (prefix) => new RegExp(`node_modules[\\/]${prefix}`)
-    )
+  ? bpkReactScriptsConfig.babelIncludePrefixes.map(prefix => {
+      if (prefix && (prefix.startsWith('.') || prefix.startsWith('/'))) {
+        // if the prefixes starts with '.', '..' or '/', likes '../common'
+        // it means is the relative path which doesn't need to be in the node_modules folder
+        console.log(path.resolve(paths.appPath, prefix));
+        return path.resolve(paths.appPath, prefix);
+      }
+      return new RegExp(`node_modules[\\/]${prefix}`);
+    })
   : [];
 
 // Backpack / saddlebag node module regexes
