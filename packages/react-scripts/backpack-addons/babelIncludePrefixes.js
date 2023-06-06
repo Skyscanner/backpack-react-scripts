d5c10b7d488e2e4c9c1558a80214ce416682e3e3
gc.zhu <gc.zhu@skyscanner.net>
2023-06-06 11:00:56 +0800

    unformat

--- packages/react-scripts/backpack-addons/babelIncludePrefixes.js
+++ packages/react-scripts/backpack-addons/babelIncludePrefixes.js
@@ -1,18 +1,18 @@
-'use strict';
+"use strict";
 
-const paths = require('../config/paths');
+const paths = require("../config/paths");
 const path = require('path');
 const appPackageJson = require(paths.appPackageJson);
-const bpkReactScriptsConfig = appPackageJson['backpack-react-scripts'] || {};
+const bpkReactScriptsConfig = appPackageJson["backpack-react-scripts"] || {};
 const customModuleRegexes = bpkReactScriptsConfig.babelIncludePrefixes
   ? bpkReactScriptsConfig.babelIncludePrefixes.map(prefix => {
-      if (prefix && /^(\.|\/)/.test(prefix)) {
-        // if the prefixes starts with '.' or '/', e.g. '../common' './common' '/common'
-        // That means it is a relative path which doesn't need to be in the node_modules folder
-        return path.resolve(paths.appPath, prefix);
-      }
-      return new RegExp(`node_modules[\\/]${prefix}`);
-    })
+    if (prefix && /^(\.|\/)/.test(prefix)) {
+      // if the prefixes starts with '.' or '/', e.g. '../common' './common' '/common'
+      // That means it is a relative path which doesn't need to be in the node_modules folder
+      return path.resolve(paths.appPath, prefix);
+    }
+    return new RegExp(`node_modules[\\/]${prefix}`);
+  })
   : [];
 
 // Backpack / saddlebag node module regexes
