70096560c0558906349c09215bcda47bb8f39255
James ODonnell <james.odonnell@skyscanner.net>
2023-10-11 09:16:58 +0100

    Add tests

--- packages/react-scripts/backpack-addons/splitChunks.js
+++ packages/react-scripts/backpack-addons/splitChunks.js
@@ -1,14 +1,8 @@
-'use strict';
-
-const paths = require('../config/paths');
-const appPackageJson = require(paths.appPackageJson);
-const bpkReactScriptsConfig = appPackageJson['backpack-react-scripts'] || {};
-
 /**
  * Defines a webpack splitChunks configuration, optionally based on consumer configuration.
- * 
+ *
  * For automatic configuration set enableAutomaticChunking and optionally provide a vendorsChunkRegex string, e.g:
- * 
+ *
  * // package.json
  * ...
  * "backpack-react-scripts": {
@@ -18,9 +12,9 @@ const bpkReactScriptsConfig = appPackageJson['backpack-react-scripts'] || {};
  *    ...
  * }
  * ...
- * 
+ *
  * For custom configuration disable enableAutomaticChunking and provide a configuration object, e.g:
- * 
+ *
  * // package.json
  * ...
  * "backpack-react-scripts": {
@@ -43,7 +37,7 @@ const bpkReactScriptsConfig = appPackageJson['backpack-react-scripts'] || {};
  *    ...
  * }
  * ...
- * 
+ *
  * References:
  * https://webpack.js.org/plugins/split-chunks-plugin/#optimizationsplitchunks
  * https://webpack.js.org/plugins/split-chunks-plugin/#splitchunkscachegroups
@@ -51,9 +45,14 @@ const bpkReactScriptsConfig = appPackageJson['backpack-react-scripts'] || {};
  * https://medium.com/webpack/webpack-4-code-splitting-chunk-graph-and-the-splitchunks-optimization-be739a861366
  */
 
-module.exports = (isEnvDevelopment) => {
+'use strict';
 
-  let splitChunksConfig =  {}
+const paths = require('../config/paths');
+const appPackageJson = require(paths.appPackageJson);
+const bpkReactScriptsConfig = appPackageJson['backpack-react-scripts'] || {};
+
+module.exports = isEnvDevelopment => {
+  let splitChunksConfig = {};
 
   // If opted in to automatic chunking, apply default configuration
   if (bpkReactScriptsConfig.enableAutomaticChunking) {
@@ -73,16 +72,22 @@ module.exports = (isEnvDevelopment) => {
     }
   }
   // If not opted in to automatic chunking, use custom configuration - if defined.
-  else if(bpkReactScriptsConfig.splitChunksConfig) {
-    // 
-    splitChunksConfig = { ...bpkReactScriptsConfig.splitChunksConfig, name: isEnvDevelopment}
-    // Regexes are passed as strings in package.json config, but need constructed here.
-    for(let cacheGroup of Object.keys(splitChunksConfig.cacheGroups)){
-      splitChunksConfig.cacheGroups[cacheGroup].test = new RegExp(splitChunksConfig.cacheGroups[cacheGroup].test);
+  else if (bpkReactScriptsConfig.splitChunksConfig) {
+    splitChunksConfig = {
+      ...bpkReactScriptsConfig.splitChunksConfig,
+      name: isEnvDevelopment,
+    };
+    if (splitChunksConfig.cacheGroups) {
+      // Regexes are passed as strings in package.json config, but need constructed here.
+      for (let cacheGroup of Object.keys(splitChunksConfig.cacheGroups)) {
+        splitChunksConfig.cacheGroups[cacheGroup].test = new RegExp(
+          splitChunksConfig.cacheGroups[cacheGroup].test
+        );
+      }
     }
   }
 
   return {
-    splitChunks: splitChunksConfig
+    splitChunks: splitChunksConfig,
   };
 };
