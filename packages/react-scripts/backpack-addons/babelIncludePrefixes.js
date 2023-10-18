bd26396f90b03fc72dbf7378cbba425be0a3aa09
Alex Sannikov <23205951+xalechez@users.noreply.github.com>
2023-10-18 09:43:36 +0100

    [ARGG-774]: Sync with main branch (#170)
    * [ARGG-774]: Sync with main branch
    
    * ARGG-774: lock file
    
    * ARGG-774: lock file 2
    
    ---------
    
    Co-authored-by: Aleksandr Sannikov <aleksandr.sannikov@skyscanner.net>

--- packages/react-scripts/backpack-addons/babelIncludePrefixes.js
+++ packages/react-scripts/backpack-addons/babelIncludePrefixes.js
@@ -1,18 +1,26 @@
 'use strict';
 
 const paths = require('../config/paths');
+const path = require('path');
 const appPackageJson = require(paths.appPackageJson);
 const bpkReactScriptsConfig = appPackageJson['backpack-react-scripts'] || {};
 const customModuleRegexes = bpkReactScriptsConfig.babelIncludePrefixes
-  ? bpkReactScriptsConfig.babelIncludePrefixes.map(
-      prefix => new RegExp(`node_modules[\\/]${prefix}`)
-    )
+  ? bpkReactScriptsConfig.babelIncludePrefixes.map(prefix => {
+      if (prefix && /^(\.|\/)/.test(prefix)) {
+        // if the prefixes starts with '.' or '/', e.g. '../common' './common' '/common'
+        // That means it is a relative path which doesn't need to be in the node_modules folder
+        return path.resolve(paths.appPath, prefix);
+      }
+      return new RegExp(`node_modules[\\/]${prefix}`);
+    })
   : [];
 
 // Backpack / saddlebag node module regexes
 const backpackModulesRegex = /node_modules[\\/]bpk-/;
 const saddlebagModulesRegex = /node_modules[\\/]saddlebag-/;
 const scopedBackpackModulesRegex = /node_modules[\\/]@skyscanner[\\/]bpk-/;
+const backpackSinglePackageModulesRegex =
+  /node_modules[\\/]@skyscanner[\\/]backpack-web/;
 
 module.exports = () => {
   return [
@@ -20,6 +28,7 @@ module.exports = () => {
     backpackModulesRegex,
     saddlebagModulesRegex,
     scopedBackpackModulesRegex,
+    backpackSinglePackageModulesRegex,
     ...customModuleRegexes,
   ];
 };
