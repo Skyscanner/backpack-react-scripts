8d8abfc3983fcf2303123ac80fb71382fefc8a0a
Mungo Dewar <mungodewar@skyscanner.net>
2025-10-09 11:01:57 +0100

    fix isLocal

--- packages/react-scripts/backpack-addons/ssr/isLocalComponents.js
+++ packages/react-scripts/backpack-addons/ssr/isLocalComponents.js
@@ -6,10 +6,9 @@ const paths = require('../../config/paths');
  * Checking for loadable install, as there is need to set aliases when this is the case
  */
 const isLocalComponents = () => {
-  // Ensure `ssrEnabled` is on
   const appPackageJson = require(paths.appPackageJson);
   const isLocalComponentsInstalled =
-    appPackageJson['@skyscanner-internal/local-components'] || {};
+    appPackageJson['@skyscanner-internal/local-components'];
 
   return isLocalComponentsInstalled;
 };
